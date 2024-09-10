64d11e39dd35439ec76412ea5ce4f9deade5cd5f
“Iryna <maria14trush@gmail.com>
2024-09-10 12:28:49 -0700

    add cards with articles to the component, create scss file to provide styling

--- src/pages/articles/Articles.jsx
+++ src/pages/articles/Articles.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import Card from "../projects/components/card";
+import { Link } from "react-router-dom";
+import "./articles.scss";
 
 export default function Articles() {
+  const cardData = [
+    {
+      cardTitle: "Convert Text to Speech with Python",
+      cardDescription: "A Step-by-Step Guide for Developers",
+      cardField: "",
+      link: "https://medium.com/@trushmi415/convert-text-to-speech-with-python-b0f0d43a4ad9",
+      isExternal: true,
+    },
+  ];
   return (
-    <div>
-      Thoughts on topics ranging from coding challenges to software development
-      best practices I have learned.
+    <div className="main-container">
+      <div className="description-section-container">
+        Thoughts on topics ranging from coding challenges to software
+        development best practices I have learned.
+      </div>
+      <div className="card-container">
+        {cardData.map(
+          (
+            { cardTitle, cardDescription, cardField, link, isExternal },
+            index
+          ) =>
+            isExternal ? (
+              <a href={link} className="projects-link" key={index}>
+                <Card
+                  cardTitle={cardTitle}
+                  cardDescription={cardDescription}
+                  cardField={cardField}
+                />
+              </a>
+            ) : (
+              <Link to={link} className="projects-link" key={index}>
+                <Card
+                  cardTitle={cardTitle}
+                  cardDescription={cardDescription}
+                  cardField={cardField}
+                />
+              </Link>
+            )
+        )}
+      </div>
     </div>
   );
 }
