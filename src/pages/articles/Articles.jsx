d841a47a01274b0a37573c65d557865ad126e749
“Iryna <trushmi415@gmail.com>
2025-01-06 21:51:35 -0800

    add more links to the articles section

--- src/pages/articles/Articles.jsx
+++ src/pages/articles/Articles.jsx
@@ -7,9 +7,23 @@ export default function Articles() {
   const cardData = [
     {
       cardTitle: "Convert Text to Speech with Python",
-      cardDescription: "A Step-by-Step Guide for Developers",
-      cardField: "",
+      cardDescription: "A step-by-step guide ",
+      cardField: "Python",
       link: "https://medium.com/@trushmi415/convert-text-to-speech-with-python-b0f0d43a4ad9",
+      isExternal: true,
+    },
+    {
+      cardTitle: "How to use Free dictionary API",
+      cardDescription: "Documentation +  examples",
+      cardField: "JavaScript",
+      link: "https://dev.to/trushmi/how-to-use-free-dictionary-api-gec",
+      isExternal: true,
+    },
+    {
+      cardTitle: "7 Essential Events in React",
+      cardDescription: "Overview with code snippets",
+      cardField: "React, TypeScript",
+      link: "https://dev.to/trushmi/7-essential-events-in-react-you-need-to-know-aan",
       isExternal: true,
     },
   ];
--- src/pages/projects/components/card.jsx
+++ src/pages/projects/components/card.jsx
@@ -6,13 +6,15 @@ import "./card.scss";
 Card.propTypes = {
   cardTitle: PropTypes.string,
   cardDescription: PropTypes.string,
+  cardField: PropTypes.string,
 };
 
-export default function Card({ cardTitle, cardDescription }) {
+export default function Card({ cardTitle, cardDescription, cardField }) {
   return (
     <div className="card">
       <div className="card-logo">{cardTitle}</div>
       <div className="card-description">{cardDescription}</div>
+      <div className="card-field">{cardField}</div>
     </div>
   );
 }
